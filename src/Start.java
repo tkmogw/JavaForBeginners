42a094cc6e80070ab1de397b93f02588577d166b
LAPTOP-5FDSIJ9O\takum <zeal24360223@fsgcl.jp>
2025-05-20 12:18:01 +0900

    加筆start 追加RectTest.java

--- src/Start.java
+++ src/Start.java
@@ -3,7 +3,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-System.out.println("きんたま");
+        System.out.println("きんたま");
+        MyFrame frame1=new RectTest();
 	}
 
 }
