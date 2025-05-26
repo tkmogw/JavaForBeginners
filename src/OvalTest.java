1d4fa5eac2c78afe501276d8a2d32b5f4248b1de
LAPTOP-5FDSIJ9O\takum <zeal24360223@fsgcl.jp>
2025-05-26 11:18:21 +0900

    ミスってた

--- src/OvalTest.java
+++ src/OvalTest.java
@@ -3,7 +3,7 @@ public class OvalTest extends MyFrame {
 
 	public void run() {
 		fillOval(30,40,50,100);
-		fillOval(120,40,50,100);
+		fillOval(130,40,50,100);
 		fillRect(30,200,150,10);
 		// TODO 自動生成されたメソッド・スタブ
 
