79422a994830a254890550c6eac5959e043f5ecc
LAPTOP-5FDSIJ9O\takum <zeal24360223@fsgcl.jp>
2025-05-26 10:10:34 +0900

    課題

--- src/RectTest.java
+++ src/RectTest.java
@@ -2,10 +2,18 @@
 public class RectTest extends MyFrame {
 
 	public void run () {
+		setColor(255,255,0);
 		fillRect(30,40,50,100);
+		setColor(128,0,128);
 		fillRect(130,40,50,100);
+		setColor(0,255,255);
 		fillRect(30,200,150,10);
+		setColor(0,0,0);
 		fillRect(90,160,30,30);
+		setColor(128,128,128);
+		fillRect(40,60,30,30);
+		setColor(255,255,255);
+		fillRect(40,100,30,30);
 	}
 		// TODO 自動生成されたメソッド・スタブ
 
