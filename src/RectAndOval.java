2bfb0a046fa2d535129ce2e514847ac87c0b7308
LAPTOP-5FDSIJ9O\takum <zeal24360223@fsgcl.jp>
2025-05-26 11:17:53 +0900

    練習問題3-1

---
+++ src/RectAndOval.java
@@ -0,0 +1,15 @@
+
+public class RectAndOval extends MyFrame {
+
+	public void run() {
+		//setColor(255,218,185);
+		fillOval(50,170,30,30);
+		fillOval(120,170,30,30);
+		fillRect(30,140,130,30);
+		//fillRect(70,10,60,130);
+		fillRect(70,110,60,30);
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
