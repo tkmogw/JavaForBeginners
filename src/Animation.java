2e90e1f76d9505ce15fb86a4177ad43d0149f6e0
LAPTOP-5FDSIJ9O\takum <zeal24360223@fsgcl.jp>
2025-05-27 10:50:46 +0900

    課題5-2

---
+++ src/Animation.java
@@ -0,0 +1,17 @@
+
+public class Animation extends MyFrame {
+
+	public void run() {
+       
+       int x=30;
+       while (x<=170) {
+    	   clear();
+    	   setColor(0,128,0);
+    	   fillOval (x,80,100,100);
+    	   x+=20;
+    	   sleep(1);
+       }
+
+	}
+
+}
