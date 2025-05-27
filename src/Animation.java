215ba90de2529b5e3220d797ce7c0fe494e78260
LAPTOP-5FDSIJ9O\takum <zeal24360223@fsgcl.jp>
2025-05-27 13:27:48 +0900

    課題５　EX1まで

--- src/Animation.java
+++ src/Animation.java
@@ -3,15 +3,128 @@ public class Animation extends MyFrame {
 
 	public void run() {
        
-       int x=30;
-       while (x<=170) {
-    	   clear();
-    	   setColor(0,128,0);
-    	   fillOval (x,80,100,100);
-    	   x+=5;
-    	   sleep(0.02);
-       }
+       int y=100;
+		   int x=30;
+		   int n=0;
+		   //5-1
+		//while (x<=100) {
+		//   clear();
+		// setColor(0,128,0);
+    	   //  fillRect (30,170-x,100,100);
+		//	   x++;
+    	   //	 
+		// 	   sleep(0.01);
+		//    }
+       //ここでx=170、y座標は30
+		//    x=30;
+		//   while (x<=100) {
+		//	   clear();
+		//	   setColor(0,0,0);
+		//	   fillRect (x,70,100,100);
+//	   x+=2;
+    	 
+    //	   sleep(0.01);
+		   //}
+    //   }
+       
 
+//5-2
+		   //while (x<=170) {
+		   //   clear();
+		   		   // setColor(0,128,0);
+		   //    fillRect (x,y,10,100);
+		   //	  x+=5;
+	  //	  y+=5;
+	  //	   sleep(0.01);
+	   //}
+		   //while (x>=30) {
+		   //	   clear();
+		   //	 setColor(0,128,0);
+		   //	     fillRect (x,y,10,100);
+		   //		  x-=5;
+	
+		   //		   sleep(0.01);
+		   //	}
+		   //while (x<=170) {
+		   //	   clear();
+		   //	 setColor(0,128,0);
+		   //	     fillRect (x,y,10,100);
+		   //		  x+=5;
+		   		   //		  y-=5;
+		   //	   sleep(0.01);
+		   //}
+		   //while (x>=30) {
+		   //	   clear();
+		   // setColor(0,128,0);
+		   //	     fillRect (x,y,10,100);
+		   //	  x-=5;
+		   //	   sleep(0.01);
+		   //	}
+		 //5-3
+		   // while(n<=2)
+			   
+		   //	   {
+			   
+		   //while (x<=170) {
+		   //	   	   clear();
+		   	//	   	 setColor(0,128,0);
+		   //	        fillRect (x,y,10,100);
+		   //      x+=5;
+	       
+		   //		   	   sleep(0.01);
+		   //		   }
+	
+		   //	while (x>=30) {
+		   //		   clear();
+		   //		 setColor(0,128,0);
+		   //	     fillRect (x,y,10,100);
+		   //	  x-=5;
+			  //		  sleep(0.01);
+		
+			  //	}
+	//	n++;
+	
+	
+		   // }
+		   //EX1
+		   while(true) {
+			   while (x<=70) {
+				      clear();
+				   		    setColor(0,128,0);
+				       fillRect (x,y,10,100);
+				   	  x+=5;
+				   	
+			  	   sleep(0.01);
+		   }
+			   while (y<=200) {
+				      clear();
+				   		    setColor(0,28,0);
+				       fillRect (x,y,10,100);
+				   	  y+=5;
+			  	  
+			  	   sleep(0.01);
+		   }
+			   while (x>=30) {
+				      clear();
+				   		    setColor(130,18,0);
+				       fillRect (x,y,10,100);
+				   	  x-=5;
+			  	  
+			  	   sleep(0.01);
+		   }
+			   while (y>=70) {
+				      clear();
+				   		    setColor(0,12,0);
+				       fillRect (x,y,10,100);
+				   	  y+=5;
+			  	  
+			  	   sleep(0.01);
+		   }
 	}
-
+	
 }
+}
+
+
+
+
