525e13c711876e9d1770b259e56168bacd5c7ef5
LAPTOP-5FDSIJ9O\takum <zeal24360223@fsgcl.jp>
2025-06-03 09:49:07 +0900

    練習問題6-1,6-2

---
+++ src/Car.java
@@ -0,0 +1,33 @@
+
+public class Car {
+
+
+	int x,y,vx,vy;
+	   public Car(int x,int y,int vx,int vy)
+	   {
+		   this.x=x;
+		   this.y=y;
+		   this.vx=vx;
+		   this.vy=vy;
+	   }
+		public void draw(MyFrame frame) {
+			frame.fillRect(x+10,y+20,50,30);
+			frame.fillRect(x-20,y+50,110,30);
+			frame.fillOval(x,y+79,25,25);
+			frame.fillOval(x+50,y+79,25,25);
+			// TODO 自動生成されたメソッド・スタブ
+
+		}
+		public void move() {
+			x+=vx;
+		
+			
+		}
+		// TODO 自動生成されたメソッド・スタブ
+
+	
+
+
+	}
+
+
